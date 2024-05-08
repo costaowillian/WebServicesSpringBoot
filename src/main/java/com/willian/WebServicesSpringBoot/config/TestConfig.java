9d0d8d92553c8fee7bda3a7d149b17368129bc7d
Willian Costa <willian95@outlook.com>
2024-05-08 14:49:47 -0300

    feat: add enum OrderStatus

--- src/main/java/com/willian/WebServicesSpringBoot/config/TestConfig.java
+++ src/main/java/com/willian/WebServicesSpringBoot/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.willian.WebServicesSpringBoot.entities.Order;
 import com.willian.WebServicesSpringBoot.entities.User;
+import com.willian.WebServicesSpringBoot.entities.enums.OrderStatus;
 import com.willian.WebServicesSpringBoot.repositories.OrderRepository;
 import com.willian.WebServicesSpringBoot.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 				
-		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1, OrderStatus.PAID);
+		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2, OrderStatus.WAITING_PAYMENT);
+		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1, OrderStatus.WAITING_PAYMENT);
 		
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
