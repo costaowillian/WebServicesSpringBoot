5b3fa3c92049adf9ce36595efad4f9f187f0aace
Willian Costa <willian95@outlook.com>
2024-05-08 14:16:05 -0300

    feat: add Order

--- src/main/java/com/willian/WebServicesSpringBoot/entities/User.java
+++ src/main/java/com/willian/WebServicesSpringBoot/entities/User.java
@@ -1,13 +1,17 @@
 package com.willian.WebServicesSpringBoot.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,6 +36,9 @@ public class User implements Serializable {
 	@Column(name = "password", nullable = false, length = 100)
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -80,6 +87,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
