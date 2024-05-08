d6e287ec9782e82f14d98ea9841ab5fe85823df2
Willian Costa <willian95@outlook.com>
2024-05-08 14:21:59 -0300

    feat: add OrderService, OrderRepository, OrderResource

---
+++ src/main/java/com/willian/WebServicesSpringBoot/resources/OrderResource.java
@@ -0,0 +1,39 @@
+package com.willian.WebServicesSpringBoot.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.willian.WebServicesSpringBoot.entities.User;
+import com.willian.WebServicesSpringBoot.services.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class OrderResource {
+	
+	@Autowired
+	private UserService service;
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ src/main/java/com/willian/WebServicesSpringBoot/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.willian.WebServicesSpringBoot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.willian.WebServicesSpringBoot.entities.Order;
+import com.willian.WebServicesSpringBoot.entities.User;
+import com.willian.WebServicesSpringBoot.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj =  repository.findById(id);
+		return obj.get();
+	}
+}
