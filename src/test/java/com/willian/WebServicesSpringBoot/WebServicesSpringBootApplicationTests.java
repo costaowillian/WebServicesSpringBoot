f9368c223acd1ff093d6087d68e4f5a316bf9fc1
Willian Costa <willian95@outlook.com>
2024-05-08 11:47:12 -0300

    build: project creted

---
+++ src/test/java/com/willian/WebServicesSpringBoot/WebServicesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willian.WebServicesSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
