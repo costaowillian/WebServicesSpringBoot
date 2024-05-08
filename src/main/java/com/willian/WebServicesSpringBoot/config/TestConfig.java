53100ab3a6af54fdba5e8a38676cf8647941f2cd
Willian Costa <willian95@outlook.com>
2024-05-08 14:59:30 -0300

    feat: add Category service

--- src/main/java/com/willian/WebServicesSpringBoot/config/TestConfig.java
+++ src/main/java/com/willian/WebServicesSpringBoot/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.willian.WebServicesSpringBoot.entities.Category;
 import com.willian.WebServicesSpringBoot.entities.Order;
 import com.willian.WebServicesSpringBoot.entities.User;
 import com.willian.WebServicesSpringBoot.entities.enums.OrderStatus;
+import com.willian.WebServicesSpringBoot.repositories.CategoryRepository;
 import com.willian.WebServicesSpringBoot.repositories.OrderRepository;
 import com.willian.WebServicesSpringBoot.repositories.UserRepository;
 
@@ -24,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+		
 	@Override
 	public void run(String... args) throws Exception {
 		User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -32,8 +37,14 @@ public class TestConfig implements CommandLineRunner {
 		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1, OrderStatus.PAID);
 		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2, OrderStatus.WAITING_PAYMENT);
 		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1, OrderStatus.WAITING_PAYMENT);
-		
+				
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}	
 }
