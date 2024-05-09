ace2885d69394ae941b9af00ca63d59a9af5d7e3
Willian Costa <willian95@outlook.com>
2024-05-09 10:36:49 -0300

    feat: add exception handler findById

--- src/main/java/com/willian/WebServicesSpringBoot/services/UserService.java
+++ src/main/java/com/willian/WebServicesSpringBoot/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.willian.WebServicesSpringBoot.entities.User;
 import com.willian.WebServicesSpringBoot.repositories.UserRepository;
+import com.willian.WebServicesSpringBoot.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj =  repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
