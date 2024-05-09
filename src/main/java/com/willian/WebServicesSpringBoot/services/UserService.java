07e6a66831ac28ae7ca4ef47af86d645c2a8a623
Willian Costa <willian95@outlook.com>
2024-05-09 11:00:19 -0300

    feat: add exception handler update

--- src/main/java/com/willian/WebServicesSpringBoot/services/UserService.java
+++ src/main/java/com/willian/WebServicesSpringBoot/services/UserService.java
@@ -12,6 +12,8 @@ import com.willian.WebServicesSpringBoot.repositories.UserRepository;
 import com.willian.WebServicesSpringBoot.services.exceptions.DatabaseException;
 import com.willian.WebServicesSpringBoot.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity); 
+		}
+		catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);		
+		}
 	}
 
 	private void updateData(User entity, User obj) {
